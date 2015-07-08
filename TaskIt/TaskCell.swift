e67e2cd3de0dedfaadd94eb63db88ba01f437be3
Alan R <alanrandell@gmail.com>
2015-07-07 20:18:13 -0700

    Set up prototype cell

---
+++ TaskIt/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  TaskIt
+//
+//  Created by Alan Randell-Chen on 7/7/15.
+//  Copyright (c) 2015 Alan Randell-Chen. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
