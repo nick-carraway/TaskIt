da04f709b47120983822273d0132457be10478dd
Alan R <alanrandell@gmail.com>
2015-07-11 23:38:59 -0700

    added ability to uncomplete tasks

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -165,12 +165,28 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         let thisTask = baseArray[indexPath.section][indexPath.row]
+        
+        if indexPath.section == 0 {
+    
+
+        
+        
         var newTask = TaskModel(task: thisTask.task, subTask: thisTask.subTask, date: thisTask.date, completed: true)
         
-        baseArray[indexPath.section].removeAtIndex(indexPath.row)
-    
+
         baseArray[1].append(newTask)
+        
+        } else {
+            var newTask = TaskModel(task: thisTask.task, subTask: thisTask.subTask, date: thisTask.date, completed: false)
+            baseArray[0].append(newTask)
+        }
+        
+        
+        
+        baseArray[indexPath.section].removeAtIndex(indexPath.row)
+            
         tableView.reloadData()
+            
         
     }
 }
