62872b142a3e78cad91823602c863a695495de5a
Alan R <alanrandell@gmail.com>
2015-07-12 14:54:31 -0700

    Created Task Model Core Data ; next lecture to start is the creating and accessing a taskmodel

--- TaskModel.swift
+++ TaskModel.swift
@@ -2,16 +2,20 @@
 //  TaskModel.swift
 //  TaskIt
 //
-//  Created by Alan Randell-Chen on 7/7/15.
+//  Created by Alan Randell-Chen on 7/12/15.
 //  Copyright (c) 2015 Alan Randell-Chen. All rights reserved.
 //
 
 import Foundation
+import CoreData
 
-struct TaskModel {
-    var task:String
-    var subTask:String
-    var date:NSDate
-    var completed:Bool
-    
+@objc(TaskModel)
+
+class TaskModel: NSManagedObject {
+
+    @NSManaged var completed: NSNumber
+    @NSManaged var date: NSDate
+    @NSManaged var subtask: String
+    @NSManaged var task: String
+
 }
